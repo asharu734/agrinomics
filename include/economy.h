bf422847b76c0c9f2795d6f46314dfbccaae5053
Azerine <alterna811@gmail.com>
2025-04-29 09:54:45 +0800

    Add new files, organization

---
+++ include/economy.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void alertOfSuccess()
+{
+    printf("economy.h: Yippies!\n");
+}
---
+++ src/mainloop.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../include/economy.h"
+
+// Will integrate into client later.
+
+void printTitleScreen()
+{
+    printf("Hello, world!\n");
+}
+
+int main()
+{
+    printTitleScreen();
+    alertOfSuccess();
+    
+    return 0;
+}
