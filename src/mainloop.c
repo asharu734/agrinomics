8cdfe72c7a088d353ee379d32388b6e0a59b4c2b
Azerine <alterna811@gmail.com>
2025-04-30 06:52:52 +0800

    Add further tests

--- src/mainloop.c
+++ src/mainloop.c
@@ -22,6 +22,18 @@ int main()
     printf("%s\n%d\n", getCropName(&testCrop), getCropBasePrice(&testCrop));
 
     printf("What in the goddamn?\n");
+
+    int testPrice = testCrop.basePrice;
+
+    int testSupply = 46;
+    int testDemand = 0;
+    printf("Supple-Demand Ratio: %f\n", 
+        calculateRatioFromSupplyAndDemand(testSupply, testDemand));
+
+    int testCurrentPrice = testPrice
+     * calculateRatioFromSupplyAndDemand(testSupply, testDemand);
+
+    printf("Base price: %d\n New price: %d\n", testPrice, testCurrentPrice);
     
     return 0;
 }
