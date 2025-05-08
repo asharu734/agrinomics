1db1d8f9e7e7303f90d320b73070d39edd979596
Azerine <alterna811@gmail.com>
2025-05-08 19:25:08 +0800

    Add more dynamic pricing functions

--- include/economy.h
+++ include/economy.h
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "../include/crops.h"
+#include "../include/seeds.h"
+
 void alertOfSuccess()
 {
     printf("economy.h: Yippies!\n");
@@ -14,4 +17,17 @@ float calculateRatioFromSupplyAndDemand(int supply, int demand)
     float priceRatio = (float)demand / (float)supply;
 
     return priceRatio;
+}
+
+void updateCropMarketPrices(Crop* crops, int cropCount)
+{
+    for (int i = 0; i < cropCount; i++)
+    {
+        float ratio = calculateRatioFromSupplyAndDemand(
+            crops[i].supply + 1, crops[i].demand + 1);
+        float randomFactor = 0.9f + (rand() % 21) / 100.0f;
+
+        crops[i].buyingPrice = crops[i].basePrice * 0.4 * ratio * randomFactor;
+        crops[i].sellingPrice = crops[i].basePrice * ratio * randomFactor;
+    }
 }
