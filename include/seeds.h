4440942631718f5f0822656486e0aefea2c2572e
Azerine <alterna811@gmail.com>
2025-05-08 10:31:02 +0800

    Add seed initialization

--- include/seeds.h
+++ include/seeds.h
@@ -1,11 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct Seed
 {
     char* name;
-    int price;
+    int basePrice;
     int daysBeforeHarvest;
-    int minHarvestQuantity;
-    int maxHarvestQuantity;
+    int baseHarvestAmount;
 }
 Seed;
+
+Seed initSeed(char* name, int basePrice, int daysBeforeHarvest,
+              int baseHarvestAmount)
+{
+    Seed seed;
+    seed.name = strdup(name);
+    seed.basePrice = basePrice;
+    seed.daysBeforeHarvest = daysBeforeHarvest;
+    seed.baseHarvestAmount = baseHarvestAmount;
+
+    return seed;
+}
