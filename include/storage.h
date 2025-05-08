03becdc99f51a7c859a2306cd6581d7d9f04842c
Azerine <alterna811@gmail.com>
2025-05-08 14:07:17 +0800

    Initial commit

---
+++ include/storage.h
@@ -0,0 +1,71 @@
+#ifndef STORAGE_H
+#define STORAGE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "seeds.h"
+#include "crops.h"
+
+#define MAX 100
+
+typedef struct Storage
+{
+    Crop crops[MAX];
+    int cropsInStorage;
+    Seed seeds[MAX];
+    int seedsInStorage;
+}
+Storage;
+
+void initStorage(Storage* storage)
+{
+    storage->cropsInStorage = 0;
+    storage->seedsInStorage = 0;
+}
+
+int isCropsFull(Storage* storage)
+{
+    return storage->cropsInStorage == MAX;
+}
+
+int isSeedsFull(Storage* storage)
+{
+    return storage->seedsInStorage == MAX;
+}
+
+int addCropToStorage(Storage* storage, Crop crop)
+{
+    if (isCropsFull(storage))
+        return 0;
+
+    storage->crops[storage->cropsInStorage++] = crop;
+
+    return 1;
+}
+
+int addSeedToStorage(Storage* storage, Seed seed)
+{
+    if (isSeedsFull(storage))
+        return 0;
+
+    storage->seeds[storage->seedsInStorage++] = seed;
+
+    return 1;
+}
+
+void freeStorage(Storage* storage)
+{
+    for (int i = 0; i < storage->cropsInStorage; i++)
+    {
+        freeCrop(&storage->crops[i]);
+    }
+
+    for (int i = 0; i < storage->seedsInStorage; i++)
+    {
+        freeSeed(&storage->seeds[i]);
+    }
+}
+
+#endif
