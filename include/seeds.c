6df887ead11ca9f4f5404c45609d1010ee1c8b79
Azerine <alterna811@gmail.com>
2025-05-10 14:45:07 +0800

    Separated seeds

---
+++ include/seeds.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "include/seeds.h"
+
+Seed initSeed(char* name, int basePrice, int daysBeforeHarvest)
+{
+    Seed seed;
+    seed.name = strdup(name);
+    seed.basePrice = basePrice;
+    seed.daysBeforeHarvest = daysBeforeHarvest;
+    seed.baseHarvestAmount = 30;
+
+    return seed;
+}
+
+char* getSeedName(Seed* seed)
+{
+    return seed->name;
+}
+
+int getSeedBasePrice(Seed* seed)
+{
+    return seed->basePrice;
+}
+
+int getSeedDaysBeforeHarvest(Seed* seed)
+{
+    return seed->daysBeforeHarvest;
+}
+
+int getSeedHarvestAmount(Seed* seed)
+{
+    return seed->baseHarvestAmount;
+}
+
+void freeSeed(Seed* seed)
+{
+    free(seed->name);
+}
