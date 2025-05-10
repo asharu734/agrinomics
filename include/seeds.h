79ef051a903bbcdea82033e4b70a98eeac06cfd9
Azerine <alterna811@gmail.com>
2025-05-10 14:44:51 +0800

    Separated seeds.h into two

--- include/seeds.h
+++ include/seeds.h
@@ -12,43 +12,19 @@ typedef struct Seed
     int daysBeforeHarvest;
     int baseHarvestAmount;
 }
-Seed;
+Seed;                        
 
 Seed initSeed(char* name, int basePrice, int daysBeforeHarvest,
-              int baseHarvestAmount)
-{
-    Seed seed;
-    seed.name = strdup(name);
-    seed.basePrice = basePrice;
-    seed.daysBeforeHarvest = daysBeforeHarvest;
-    seed.baseHarvestAmount = baseHarvestAmount;
+              int baseHarvestAmount);
 
-    return seed;
-}
+char* getSeedName(Seed* seed);
 
-char* getSeedName(Seed* seed)
-{
-    return seed->name;
-}
+int getSeedBasePrice(Seed* seed);
 
-int getSeedBasePrice(Seed* seed)
-{
-    return seed->basePrice;
-}
+int getSeedDaysBeforeHarvest(Seed* seed);
 
-int getSeedDaysBeforeHarvest(Seed* seed)
-{
-    return seed->daysBeforeHarvest;
-}
+int getSeedHarvestAmount(Seed* seed);
 
-int getSeedHarvestAmount(Seed* seed)
-{
-    return seed->baseHarvestAmount;
-}
-
-void freeSeed(Seed* seed)
-{
-    free(seed->name);
-}
+void freeSeed(Seed* seed);
 
 #endif
