19f391d297c7078817cb80b61cd25540973acadc
Azerine <alterna811@gmail.com>
2025-05-12 08:15:59 +0800

    Separated economy declaration from implementation

--- include/economy.h
+++ include/economy.h
@@ -1,33 +1,14 @@
+#ifndef ECONOMY_H
+#define ECONOMY_H
+
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "../include/crops.h"
 #include "../include/seeds.h"
 
-void alertOfSuccess()
-{
-    printf("economy.h: Yippies!\n");
-}
+float calculateRatioFromSupplyAndDemand(int supply, int demand);
 
-float calculateRatioFromSupplyAndDemand(int supply, int demand)
-{
-    if (demand == 0.0)
-        return 0.0;
+void updateCropMarketPrices(Crop* crops, int cropCount);
 
-    float priceRatio = (float)demand / (float)supply;
-
-    return priceRatio;
-}
-
-void updateCropMarketPrices(Crop* crops, int cropCount)
-{
-    for (int i = 0; i < cropCount; i++)
-    {
-        float ratio = calculateRatioFromSupplyAndDemand(
-            crops[i].supply + 1, crops[i].demand + 1);
-        float randomFactor = 0.9f + (rand() % 21) / 100.0f;
-
-        crops[i].buyingPrice = crops[i].basePrice * 0.4 * ratio * randomFactor;
-        crops[i].sellingPrice = crops[i].basePrice * ratio * randomFactor;
-    }
-}
+#endif
